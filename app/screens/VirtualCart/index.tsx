e217e92588dd5fe55812baac00fc7bd77fd46dff
Isha Gupta <isha.gupta1@hashedin.com>
2021-05-04 22:28:09 +0530

    Added and modified Screen Headers

---
+++ app/screens/VirtualCart/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const VirtualCart: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello, this is cart!!!!</Text>
+    </View>
+  );
+};
+
+export default VirtualCart;
